1aaa974eaa1fd90fc7dfd2c5b86755eb3376d714
cprom <chen.prom@gmail.com>
2020-03-12 20:15:58 -0700

    add db

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burger_db;
+
+INSERT INTO list (burger_name, devoured) VALUES("Bacon Cheeseburger", false);
+INSERT INTO list (burger_name, devoured) VALUES("Turkey Burger", true);
+INSERT INTO list (burger_name, devoured) VALUES("Hamburger", false);
